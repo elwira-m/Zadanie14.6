d8de12dae3dc0c491ae5830d1991e645e2d4587e
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-04-11 01:52:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 var Counter = React.createClass({
     getInitialState: function() {
-        return {
-            counter: 0
+            console.log('[getInitialState]: ustawiam stan początkowy')
+	   
+	    return {
+            	counter: 0
         };
     },
 
-    increment: function() {
+     componentWillMount() {
++        console.log('[componentWillMount]: przed renderowaniem')   
++    },
++
++    componentDidMount() {
++        console.log('[componentDidMount]: po renderowaniu')
++    },
+	
+     increment: function() {
         this.setState({
             counter: this.state.counter + 1
         });
-    },
+     },
 	
-  	decrement: function() {
-	    	this.setState({
-			      counter: this.state.counter - 1
-	  	});
-	  },
+     decrement: function() {
+	 this.setState({
++            counter: this.state.counter - 1
+  	});
+     },
 
     render: function() {
-        return (
-		      	React.createElement('div', {},
-				
-				          React.createElement('button', {onClick: this.decrement}, 'minus'),
-				          React.createElement('span', {}, 'Licznik ' + this.state.counter),
-			          	React.createElement('button', {onClick: this.increment}, 'plus'),
-			      )
-	    	);
+	  console.log('[render]: renderowanie...');
+       
+	  return (
+		React.createElement('div', {},				
++                   React.createElement('button', {onClick: this.decrement}, 'minus'),
++                   React.createElement('span', {}, 'Licznik ' + this.state.counter),
++            	    React.createElement('button', {onClick: this.increment}, 'plus'),
++               )
++        );
      }
 });
 
-var element = React.createElement(Counter);
++var element = React.createElement('div', {},
++    React.createElement(Counter),
++    React.createElement(Counter),
++    React.createElement(Counter)
++);
+
 ReactDOM.render(element, document.getElementById('app'));
